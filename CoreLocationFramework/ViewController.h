be8fd7e0080e77ca57010639286d28caa61c1c7f
AngelR4994 <xax4994@hotmail.co>
2016-09-02 18:24:03 -0500

    Get the user location

--- CoreLocationFramework/ViewController.h
+++ CoreLocationFramework/ViewController.h
@@ -7,8 +7,11 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<CLLocationManagerDelegate>
+@property (weak, nonatomic) IBOutlet UILabel *addressLabel;
+- (IBAction)getCurrentLocation:(id)sender;
 
 
 @end
